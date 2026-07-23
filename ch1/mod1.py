7177a4d43b543e936bb90fb9f48f31b9f3f4feef
sanghyeon YOO <sanghyeon0424@gmail.com>
2026-07-23 17:22:31 +0900

    update

---
+++ ch1/mod1.py
@@ -0,0 +1,27 @@
+# calc.py 모듈 사용
+# import calc
+
+# print(calc.add(2,2))
+# print(calc.sub(6,2))
+
+# from calc import add, sub, Math
+
+# print(add(2,2))
+# print(sub(4,2))
+
+# # Math는 클래스이기 때문에 math = Math() 선언
+# math = Math()
+# print(math.solv(4)) 
+
+from calc import *
+from mod3 import *
+from mod2 import *
+
+math = Math()
+print(math.solv(5))
+print(add(7,5))
+print(sub(7,5))
+max()
+
+
+
